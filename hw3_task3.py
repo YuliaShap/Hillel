6bc8b0cfca142190791c02972658ecaaeb080178
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-04 17:46:36 +0200

    Update hw3_task3.py

--- hw3_task3.py
+++ hw3_task3.py
@@ -1,2 +1,2 @@
-seq = '(etnfづzxfk｡12dt◕`1ad‿6hns‿1zQY◕Cd$y｡FtSq)Ze6?づ#2)$'
+seq: str = '(etnfづzxfk｡12dt◕`1ad‿6hns‿1zQY◕Cd$y｡FtSq)Ze6?づ#2)$'
 print(seq[::5]) #просим вывести каждый пятый элемент последовательности
