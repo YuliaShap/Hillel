156f331bc1cf3f0b070d36d9df9a74b2891302ee
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-04 13:44:52 +0200

    Update hw3_task1_2.py

--- hw3_task1_2.py
+++ hw3_task1_2.py
@@ -1,11 +1,13 @@
-year_of_birth = input('Hi! What is your year of birth?')
-actual_year = 2022
-if not year_of_birth.isdigit():
-    print('Insert the number')
+year_of_birth = input('Hi! What is your year of birth?')  # просим ввести год рождения
+actual_year = 2022                                         # указываем текущий год
+if not year_of_birth.isdigit():                             # проверяем, являются ли введенные данные числом
+    print('Insert the number')                              # если не число, просим его ввести
 else:
-    if actual_year - year_of_birth == 21:
+    year_of_birth = int(year_of_birth)                      # преобразовываем введенные данные в число
+    if actual_year - year_of_birth == 21:                   # если 21 год вывести “You should visit Holland.”
         print('You should visit Holland')
-    elif actual_year - year_of_birth > 21:
+    elif actual_year - year_of_birth > 21:                  # если больше 21 вывести “You should visit Vietnam.”
         print('You should visit Vietnam')
     else:
-        print('Travel everywhere')
+        print('Travel everywhere')                          # все остальные варианты. Вывести “Travell everywhere”
+
