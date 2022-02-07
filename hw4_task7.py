9a7e99c5aa73449a13539db3cbcf93930b64a327
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-07 13:48:05 +0200

    Update hw4_task7.py

--- hw4_task7.py
+++ hw4_task7.py
@@ -1,3 +1,3 @@
 list_with_tuple: list = [(1, 2), (2, 3, 5), (3, 4), (2, 3, 4, 2)]  # получаем список кортежей
-new_list = list(map(list, list_with_tuple))  # каждый кортеж в спюиске приобразовываем в список
+new_list = list(map(list, list_with_tuple))  # каждый кортеж в списке приобразовываем в список
 print(new_list)  # выводим список списков
