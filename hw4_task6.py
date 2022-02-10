9644ea85b48e44e41abe96072e2f0336e9053f88
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-10 09:06:28 +0200

    Update hw4_task6.py

--- hw4_task6.py
+++ hw4_task6.py
@@ -1,5 +1,5 @@
-new_tuple: tuple = ('1', '2', '3')  # пишем кортеж
+new_tuple: tuple = (1, 2, 3)  # пишем кортеж
 list_tuple = list(new_tuple)  # т.к. кортеж неизменяемый список, приобразовываем его в обычный список
-list_tuple.remove('3')  # удаляем элемент '3'
+list_tuple.remove(3)  # удаляем элемент '3'
 new_tuple = tuple(list_tuple)  # преобразовываем список обратно в кортеж
 print(new_tuple)  # выводим кортеж
