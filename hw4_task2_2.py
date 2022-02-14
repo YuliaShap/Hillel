fc1f9129b49b7063f7ef16f16128424eb0f8a37c
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-14 18:50:44 +0200

    Update and rename hw4_task2_2.py to hw4_task2.py

--- hw4_task2_2.py
+++
@@ -1,9 +0,0 @@
-your_string: str = str('Hillel school')
-counter = 0
-for i in your_string:
-    for j in your_string:
-        if i == j:
-            counter += 1
-        else:
-            counter = 1
-    print({i: counter}, end=' ')
