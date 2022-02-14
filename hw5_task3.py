c1c4709c40c292916bc1a0cc7de2c6753549bf4c
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-14 22:05:15 +0200

    Add files via upload

---
+++ hw5_task3.py
@@ -0,0 +1,37 @@
+str_lst = [12, 22, 66, 44, 76, 534]
+counter = 0
+for i in str_lst:
+    while i % 2 == 0:
+        print('четные')
+        counter += 1
+        break
+    else:
+        print('нечетные')
+
+
+ #if i % 2 != 0:
+
+           # break
+
+#if str_lst.isdigit
+#user_lst = map(int, str_lst.split(","))
+
+#for x in str_lst:
+    #int_lst = int(x)
+#for x in range(int_lst):
+    #if int_lst[x] % 2 == 0:
+       # print('all numbers are even')
+    #else:
+        #print('NO')
+
+
+
+#print(int_list)
+
+
+
+#for i in range(len(integ)):
+    #if integ[i] % 2 == 0:
+        #print('all numbers are even')
+    #else:
+        #print('NO')
