072dd7bb0ea3a98efaa379f0b1f48f07d99b9ed9
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-15 09:35:59 +0200

    Update hw5_task3.py

--- hw5_task3.py
+++ hw5_task3.py
@@ -1,37 +1,11 @@
-str_lst = [12, 22, 66, 44, 76, 534]
-counter = 0
-for i in str_lst:
-    while i % 2 == 0:
-        print('четные')
-        counter += 1
-        break
-    else:
-        print('нечетные')
-
-
- #if i % 2 != 0:
-
-           # break
-
-#if str_lst.isdigit
-#user_lst = map(int, str_lst.split(","))
-
-#for x in str_lst:
-    #int_lst = int(x)
-#for x in range(int_lst):
-    #if int_lst[x] % 2 == 0:
-       # print('all numbers are even')
-    #else:
-        #print('NO')
-
-
-
-#print(int_list)
-
-
-
-#for i in range(len(integ)):
-    #if integ[i] % 2 == 0:
-        #print('all numbers are even')
-    #else:
-        #print('NO')
+# просим ввести список с числами
+random_list: list = list(map(int, input('Введите произвольный список с числами').split(', ')))
+counter = 0  # устанавливаем счетчик
+while counter < len(random_list):  # в цикле идем по всем значениям, которые меньше длины списка
+    if random_list[counter] % 2 == 0:  # если число четное
+        counter += 1  # увеличиваем счетчик на 1
+    elif random_list[counter] % 2 != 0:  # если число нечетное
+        print('NO')  # принтим НЕТ
+        break  # и обрываем цикл
+else:  # если все числа четные и счетчик стал равен длине списка
+    print('all numbers are even')  # принтим, что все числа четные
