1f3962eaeca66ed8a22d40f51b38ac9597ccb43e
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-02-23 12:17:07 +0200

    Update hw6_task5.py

--- hw6_task5.py
+++ hw6_task5.py
@@ -1,18 +1,18 @@
-"""Вернуть из dictionary все уникальные values.
-Пример
-Входные данные = [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
-                  {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
-Результат = {'S001', 'S005', 'S007', 'S002', 'S009'}
-Усложнение! +5 points
-Вернуть ту же структуру.
-После dictionary L = [{"V":"S001"}, {"V": "S002"}, {"VI": "S005"}, {"V":"S009"},{"VIII":"S007"}]"""
-
 list_with_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
                  {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
 
-res = {}
+# вернуть {'S001', 'S005', 'S007', 'S002', 'S009'}
+res = set()  # создаем пустой set
+for diction in list_with_dict:  # проходимся циклом по словарям в списке
+    for v in diction.values():  # проходимся циклом по значениям в словарях
+        res.add(v)  # добавляем значение в множество, т.к. это set, в нем будут только уникальные элементы
+print(res)  # принтим результат
+
+# L = [{"V": S001"}, {"V": "S002"}, {"VI": "S005"}, {"V":"S009"},{"VIII":"S007"}]
+res = []
+
 for diction in list_with_dict:
     for k, v in diction.items():
-        if v not in res.values():
-            res[k] = v
+        if v not in res:
+            res.append(k), res.append(v)
 print(res)
