3dce530fe0793091168324aac7daa2ab79156edf
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-04-10 11:14:56 +0300

    Update hw11_task2.py

--- hw11_task2.py
+++ hw11_task2.py
@@ -1,21 +1,21 @@
 # coding=utf-8
-def devis(a, b):  # функция деления
+def devis(a, b):  
     return a / b
 
 
-def my_new_reduce(function, iterable, start=None):  # самописный reduce
-    it = iter(iterable)  # создаем итератор
-    if start is None:  #
-        value = next(it)  #
+def my_new_reduce(function, iterable, start=None):  
+    it = iter(iterable)  
+    if start is None:  
+        value = next(it)  
     else:
-        value = start  #
-    for element in it:  #
-        value = function(value, element)  #
-    return value  #
+        value = start  
+    for element in it:  
+        value = function(value, element)  
+    return value  
 
 
-x = my_new_reduce(devis, [100, 1, 1, 1])  #
-print(x)  #
+x = my_new_reduce(devis, [100, 1, 1, 1])  
+print(x)  
 
 #################################################################
 def my_reduce(function, iterable, initializer=None):
