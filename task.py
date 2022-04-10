893ca7d3c86a5964e367c8908a0fca78a4ad436c
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2022-04-10 12:15:15 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,17 @@
+
+class Jets:
+    model = None
+    country = 0
+
+    def __init__(self, name, country, quantity):
+        self.name = name
+        self.country = country
+        self.quantity = quantity
+
+
+
+first_item=Jets("F14","USA",87)
+second_item=Jets("Mirage2000","France",35)
+
+total = first_item.quantity+second_item.quantity
+print(total)
